64ea71a36ebe31b2827c36a670f4643f776d7f14
reamey <17318077256@163.com>
2020-12-28 22:09:40 +0800

    更新

---
+++ 03-construction-02/self-actulization/webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry:{
+    index:'./src/index.js',
+    // main:'./src/main.js',
+  },
+  output:{
+    path:path.resolve(__dirname,"./dist"),
+    filename:'[name].js'
+  },
+  mode:'production',
+  plugins:[
+    new MiniCssExtractPlugin({
+      filename:'css/[name]-[contenthash].css',
+    }),
+    new htmlWebpackPlugin({
+      template:'./src/index.html',
+      filename:'index.html',
+      // path:path.resolve(__dirname,"./dist")
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolveLoader:{
+    modules: ['node_modules','myLoaders']
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:[
+          // 'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath:'../',
+            },
+          },
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test:/\.less$/,
+        use:[
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath:'../',
+            },
+          },
+          // 'style-loader',
+          'css-loader',
+          // 'postcss-loader',
+          'less-loader'
+        ]
+      },
+      // {
+      //   test:/\.less$/,
+      //   use:[
+      //     'clm-style-loader',
+      //     'clm-css-loader',
+      //     'clm-less-loader'
+      //   ]
+      // },
+      {
+        test:/\.(png|jpg|jpeg|gif)$/i,
+        use:[
+          {
+            loader:'url-loader',
+            options:{
+              limit:2048,
+              outputPath:'images',
+              publicPath:'../',
+              // outputPath: "images/",
+              // publicPath: "../images",
+              filename:'[name]-[contenthash].[ext]'
+            }
+          }
+        ]
+      },
+      // {
+      //   test:/\.js$/,
+      //   // use:path.resolve(__dirname,'./myLoaders/replace-loader.js')
+      //   use:[
+      //     {
+      //       loader:'replace-loader',
+      //       options:{
+      //         info:'reamey'
+      //       }
+      //     },
+      //     {
+      //       loader:'replace-async-loader',
+      //       options:{
+      //         info:'你是'
+      //       }
+      //     }
+      //   ]
+      // }
+    ]
+  }
+}
